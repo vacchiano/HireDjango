e0c0ad54b5122a83dee61e593d1154d6135f8262
Dom Vacchiano <djvacchiano@gmail.com>
2022-08-15 17:36:00 -0500

    updated setting for deployment

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
