24d38e8d65b3f7eb03a1193fcf2fcda688caf8d9
Robert Coffie <34107104+rcoffie@users.noreply.github.com>
2022-08-22 19:37:10 +0000

    adding search feature (#6)
    * adding search feature
    
    * fixed url naming
    
    * Delete 0002_alter_customuser_groups_and_more.py (unsure why this migration file created)
    
    * Delete 0011_alter_freelancer_role_level_and_more.py (unsure why this migration file created)
    
    Co-authored-by: Robert Coffie <rcoffie22@yhahoo.com>
    Co-authored-by: Dom Vacchiano <djvacchiano@gmail.com>

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 
-from .views import (index, pricing, profile, about, BusinessCreateView, 
+from .views import (index, pricing, profile, about, BusinessCreateView,
                     FreelancerCreateView, FreelancerUpdateView,
-                    FreelancerDetailView, FreelancerListView, handle_login)
+                    FreelancerDetailView, handle_login, list_developers,)
 
 urlpatterns = [
     path('', index, name='home'),
     path('profile/', profile, name='profile'),
     path('about/', about, name='about'),
     path('pricing/', pricing, name='pricing'),
-    path('developers/', FreelancerListView.as_view(), name='freelancer-list'),
+    # path('developers/', FreelancerListView.as_view(), name='freelancer-list'),
     path('account-setup/', handle_login, name='handle-login'),
     path('developer/<int:pk>/', FreelancerDetailView.as_view(), name='freelancer-detail'),
     path('developer/create/', FreelancerCreateView.as_view(), name="freelancer-create"),
     path('developer/update/', FreelancerUpdateView.as_view(), name="freelancer-update"),
     path('business/create/', BusinessCreateView.as_view(), name="business-create"),
+    path('developers/',list_developers,name="list-developers"),
 ]
