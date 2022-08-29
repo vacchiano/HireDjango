ae4af1e0d70560fee907c84e4bf19858db9af267
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-08-29 15:57:56 +0530

    Changed freelancer detail URL and views to use username instead of pk

--- jobs/urls.py
+++ jobs/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('pricing/', pricing, name='pricing'),
     # path('developers/', FreelancerListView.as_view(), name='freelancer-list'),
     path('account-setup/', handle_login, name='handle-login'),
-    path('developer/<int:pk>/', FreelancerDetailView.as_view(), name='freelancer-detail'),
     path('developer/create/', FreelancerCreateView.as_view(), name="freelancer-create"),
     path('developer/update/', FreelancerUpdateView.as_view(), name="freelancer-update"),
     path('business/create/', BusinessCreateView.as_view(), name="business-create"),
     path('developers/',list_developers,name="list-developers"),
+    path('developer/<slug:username>/', FreelancerDetailView.as_view(), name='freelancer-detail'),
 ]
