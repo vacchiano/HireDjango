ca1afaddd2b80a729e4fa86d101e745d9a0418f3
Robert Coffie <rcoffie22@yhahoo.com>
2022-09-02 10:01:39 +0000

    removing commented code

--- jobs/tests/test_models.py
+++ jobs/tests/test_models.py
@@ -33,28 +33,3 @@ class TestFreelanceModel(TestCase):
     def test_business_model_str(self):
         self.assertEqual(self.business.owner, self.user)
         self.assertEqual(str(self.business), '1 | my_bus')
-
-# class TestRoleModel(TestCase):
-#
-#     def test_role_str(self):
-#         role = Role.objects.create(name="contract")
-#         self.assertEqual(str(role), "contract")
-#
-# class TestLevelModel(TestCase):
-#
-#     def test_level_str(self):
-#         level = Level.objects.create(name="junior")
-#         self.assertEqual(str(level),'junior')
-#
-# class TestFreelancerModel(TestCase):
-#     def test_freelance_str(self):
-#         owner = User.objects.create_user(id=1, username='max', email='test@test.com', password='password')
-#         freelancer = Freelancer.objects.create(id=1, name='test', owner=owner)
-#         self.assertEqual(str(freelancer), '1 | test')
-
-
-# class TestBusinessModel(TestCase):
-#     def test_freelance_str(self):
-#         owner = User.objects.create_user(id=1, username='max', email='test@test.com', password='password')
-#         business = Business.objects.create(id=1, name='test', owner=owner)
-#         self.assertEqual(str(business), '1 | test')
